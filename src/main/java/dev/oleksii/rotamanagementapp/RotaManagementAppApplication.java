f65275c7e484137fd167ee8ad3d07d9e9e94b571
Oleksii <aleksey.shvoruk@gmail.com>
2024-12-20 18:00:35 +0000

    Initial commit

---
+++ src/main/java/dev/oleksii/rotamanagementapp/RotaManagementAppApplication.java
@@ -0,0 +1,13 @@
+package dev.oleksii.rotamanagementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RotaManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RotaManagementAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/oleksii/rotamanagementapp/RotaManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.oleksii.rotamanagementapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RotaManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
