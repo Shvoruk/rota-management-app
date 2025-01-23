697a5e5ebb93112ec0ba6b3c6695f3f6cd95259c
Oleksii <aleksey.shvoruk@gmail.com>
2025-01-23 22:21:42 +0000

    create dtos for registration, authentication

---
+++ src/main/java/dev/oleksii/rotamanagementapp/domain/dtos/RegisterRequest.java
@@ -0,0 +1,37 @@
+package dev.oleksii.rotamanagementapp.domain.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+
+    @NotBlank(message = "First name is required.")
+    private String firstname;
+
+    @NotBlank(message = "Last name is required.")
+    private String lastname;
+
+    @NotBlank(message = "Email is required.")
+    @Email(message = "Email format is invalid.")
+    private String email;
+
+    @NotBlank(message = "Password is required.")
+    @Size(min = 8, message = "Password must be at least 8 characters long.")
+    @Pattern(
+            regexp = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$",
+            message = "Password must contain at least one digit, one uppercase letter, one lowercase letter, one special character, and no whitespace."
+    )
+    private String password;
+}
+
+
