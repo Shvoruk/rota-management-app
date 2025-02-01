f2d9ca25be224893c9afbeaf10c8641ee61a23d3
Oleksii <aleksey.shvoruk@gmail.com>
2025-02-01 23:05:29 +0000

    create delete team endpoint

--- src/main/java/dev/oleksii/rotamanagementapp/controllers/TeamController.java
+++ src/main/java/dev/oleksii/rotamanagementapp/controllers/TeamController.java
@@ -1,26 +1,38 @@
 package dev.oleksii.rotamanagementapp.controllers;
 
-import dev.oleksii.rotamanagementapp.domain.dtos.CreateTeamRequest;
 import dev.oleksii.rotamanagementapp.domain.dtos.SuccessResponse;
+import dev.oleksii.rotamanagementapp.domain.repos.UserRepository;
+import dev.oleksii.rotamanagementapp.exceptions.UserNotFoundException;
 import dev.oleksii.rotamanagementapp.services.TeamService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/teams")
 public class TeamController {
 
     private final TeamService teamService;
+    private final UserRepository userRepository;
 
-    public TeamController(TeamService teamService) {
+    public TeamController(TeamService teamService, UserRepository userRepository) {
         this.teamService = teamService;
+        this.userRepository = userRepository;
     }
 
     @PostMapping("/create")
-    public ResponseEntity<SuccessResponse> createTeam(@RequestBody CreateTeamRequest request, Principal principal) {
-        teamService.createTeam(principal.getName(), request);
+    public ResponseEntity<SuccessResponse> createTeam(@RequestParam String name, Principal principal) {
+        var user = userRepository.findByEmail(principal.getName()).orElseThrow(() -> new UserNotFoundException("User not found"));
+        teamService.createTeam(user, name);
         return ResponseEntity.ok(new SuccessResponse("Team created successfully"));
+    }
+
+    @DeleteMapping("/delete/{teamId}")
+    public ResponseEntity<SuccessResponse> deleteTeam(@PathVariable UUID teamId, Principal principal) {
+        var user = userRepository.findByEmail(principal.getName()).orElseThrow(() -> new UserNotFoundException("User not found"));
+        teamService.deleteTeam(user, teamId);
+        return ResponseEntity.ok(new SuccessResponse("Team deleted successfully"));
     }
 }
