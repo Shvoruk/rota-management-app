167dd0f1f43e108349af4060f530346a0da66e86
Oleksii <aleksey.shvoruk@gmail.com>
2025-02-01 14:12:26 +0000

    update team entity

--- src/main/java/dev/oleksii/rotamanagementapp/domain/entities/Team.java
+++ src/main/java/dev/oleksii/rotamanagementapp/domain/entities/Team.java
@@ -1,16 +1,15 @@
 package dev.oleksii.rotamanagementapp.domain.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
 @Entity
-@Data
+@Getter
+@Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
@@ -19,15 +18,28 @@ public class Team {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
+    @EqualsAndHashCode.Include
     private UUID id;
 
     @Column(nullable = false)
     private String name;
 
     @OneToMany(mappedBy = "team", cascade = {CascadeType.PERSIST, CascadeType.MERGE}, fetch = FetchType.LAZY)
-    private List<TeamMember> members;
+    @Builder.Default
+    private Set<TeamMember> members = new HashSet<>();
 
     @OneToOne(mappedBy = "team", cascade = CascadeType.ALL, optional = false)
     private Schedule schedule;
+
+    public void addMember(TeamMember member) {
+        members.add(member);
+//        member.setTeam(this);
+    }
+
+    public void removeMember(TeamMember member) {
+        members.remove(member);
+//        member.setTeam(null);
+    }
+
 
 }
