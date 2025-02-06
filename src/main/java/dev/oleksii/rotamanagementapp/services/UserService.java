49f52f6f47b0a8e6fa5eddc67b31e7de01cdb6bc
Oleksii <aleksey.shvoruk@gmail.com>
2025-02-06 22:13:28 +0000

    make userService extend userDetailsService

--- src/main/java/dev/oleksii/rotamanagementapp/services/UserService.java
+++ src/main/java/dev/oleksii/rotamanagementapp/services/UserService.java
@@ -3,9 +3,10 @@ package dev.oleksii.rotamanagementapp.services;
 import dev.oleksii.rotamanagementapp.domain.dtos.RegisterRequest;
 import dev.oleksii.rotamanagementapp.domain.dtos.UserUpdateRequest;
 import dev.oleksii.rotamanagementapp.domain.entities.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
 
     User createUser(RegisterRequest request);
     void updateUserDetailsByEmail(String email, UserUpdateRequest request);
