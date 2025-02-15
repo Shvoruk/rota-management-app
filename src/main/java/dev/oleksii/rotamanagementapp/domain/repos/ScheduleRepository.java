cd01773a51d82801768f0ad9721b52d325b58d27
Oleksii <aleksey.shvoruk@gmail.com>
2025-02-15 13:59:32 +0000

    Add methods for retrieving team IDs in repositories
    Introduced `findTeamIdByScheduleId` and `findByTeamId` in `ScheduleRepository` to enable querying team data by schedule. Added `findTeamIdByShiftId` in `ShiftRepository` for similar functionality with shifts. These changes provide streamlined access to related team information.

--- src/main/java/dev/oleksii/rotamanagementapp/domain/repos/ScheduleRepository.java
+++ src/main/java/dev/oleksii/rotamanagementapp/domain/repos/ScheduleRepository.java
@@ -2,10 +2,16 @@ package dev.oleksii.rotamanagementapp.domain.repos;
 
 import dev.oleksii.rotamanagementapp.domain.entities.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ScheduleRepository extends JpaRepository<Schedule, UUID> {
+    @Query("SELECT s.team.id FROM Schedule s WHERE s.id = :scheduleId")
+    UUID findTeamIdByScheduleId(@Param("scheduleId") UUID scheduleId);
+    Optional<Schedule> findByTeamId(UUID teamId);
 }
