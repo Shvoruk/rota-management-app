b4641a2d641ea7b0724d01f59382ea7d6b64907a
Oleksii <aleksey.shvoruk@gmail.com>
2025-02-15 15:44:49 +0000

    Refactor email configuration to use @ConfigurationProperties
    Switched from @Value annotations to @ConfigurationProperties for better modularity and consistency in handling email configuration. Updated docker-compose to pass MAIL_HOST and MAIL_PORT.

--- src/main/java/dev/oleksii/rotamanagementapp/configuration/EmailConfig.java
+++ src/main/java/dev/oleksii/rotamanagementapp/configuration/EmailConfig.java
@@ -1,6 +1,6 @@
 package dev.oleksii.rotamanagementapp.configuration;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -8,19 +8,12 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 import java.util.Properties;
 
 @Configuration
+@ConfigurationProperties(prefix = "spring.mail")
 public class EmailConfig {
 
-    // Inject from application properties
-    @Value("${spring.mail.host}")
     private String host;
-
-    @Value("${spring.mail.port}")
     private int port;
-
-    @Value("${spring.mail.username}")
     private String username;
-
-    @Value("${spring.mail.password}")
     private String password;
 
     /**
