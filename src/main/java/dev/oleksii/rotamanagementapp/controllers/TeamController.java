6e664b19e54777619592769af42b23742f7ad681
Oleksii <aleksey.shvoruk@gmail.com>
2025-02-16 12:34:29 +0000

    Refactor TeamController to improve membership validation
    Replaced AuthorizationService with MembershipService and SecurityUtil to handle membership validation more effectively. Added Principal as a parameter to several endpoints to validate user membership or role in the team. Enhanced security checks to ensure proper access control across team-related operations.

--- src/main/java/dev/oleksii/rotamanagementapp/controllers/TeamController.java
+++ src/main/java/dev/oleksii/rotamanagementapp/controllers/TeamController.java
@@ -3,7 +3,8 @@ package dev.oleksii.rotamanagementapp.controllers;
 import dev.oleksii.rotamanagementapp.domain.dtos.CreateTeamRequest;
 import dev.oleksii.rotamanagementapp.domain.dtos.MemberDto;
 import dev.oleksii.rotamanagementapp.domain.dtos.TeamDto;
-import dev.oleksii.rotamanagementapp.services.AuthorizationService;
+import dev.oleksii.rotamanagementapp.security.SecurityUtil;
+import dev.oleksii.rotamanagementapp.services.MembershipService;
 import dev.oleksii.rotamanagementapp.services.TeamService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -21,46 +22,50 @@ import java.util.UUID;
 public class TeamController {
 
     private final TeamService teamService;
-    private final AuthorizationService authorizationService;
+    private final MembershipService membershipService;
+    private final SecurityUtil securityUtil;
 
     @GetMapping
     public ResponseEntity<Set<TeamDto>> getAllTeams(Principal principal) {
-        var user = authorizationService.getCurrentUser(principal);
+        var user = securityUtil.getCurrentUser(principal);
         return ResponseEntity.ok(teamService.getAllTeams(user));
     }
 
     @GetMapping("/{teamId}")
-    public ResponseEntity<TeamDto> getTeam(@PathVariable UUID teamId) {
+    public ResponseEntity<TeamDto> getTeam(@PathVariable UUID teamId, Principal principal) {
+        membershipService.checkMembership(principal, teamId);
         return ResponseEntity.ok(teamService.getTeam(teamId));
     }
 
     @PostMapping
     public ResponseEntity<TeamDto> createTeam(@Valid @RequestBody CreateTeamRequest request, Principal principal) {
-        var user = authorizationService.getCurrentUser(principal);
+        var user = securityUtil.getCurrentUser(principal);
         return ResponseEntity.status(HttpStatus.CREATED).body(teamService.createTeam(user, request));
     }
 
     @PostMapping("/{teamId}/join")
     public ResponseEntity<TeamDto> joinTeam(@PathVariable UUID teamId, Principal principal) {
-        var user = authorizationService.getCurrentUser(principal);
+        var user = membershipService.checkNoMembership(principal, teamId);
         return ResponseEntity.ok(teamService.joinTeam(user, teamId));
     }
 
     @DeleteMapping("/{teamId}/members")
     public ResponseEntity<Void> leaveTeam(@PathVariable UUID teamId, Principal principal) {
-        var user = authorizationService.getCurrentUser(principal);
-        teamService.leaveTeam(user, teamId);
+        var member = membershipService.getMembership(principal, teamId);
+        teamService.leaveTeam(member, teamId);
         return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping("/{teamId}")
-    public ResponseEntity<Void> deleteTeam(@PathVariable UUID teamId) {
+    public ResponseEntity<Void> deleteTeam(@PathVariable UUID teamId, Principal principal) {
+        membershipService.checkManagerMembership(principal, teamId);
         teamService.deleteTeam(teamId);
         return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/{teamId}/members")
-    public ResponseEntity<Set<MemberDto>> getAllTeamMembers(@PathVariable UUID teamId) {
+    public ResponseEntity<Set<MemberDto>> getAllTeamMembers(@PathVariable UUID teamId, Principal principal) {
+        membershipService.checkMembership(principal, teamId);
         return ResponseEntity.ok(teamService.getAllTeamMembers(teamId));
     }
 
