65a7d75568523d8d7b3248af6f8a3fb0178f0b2f
Oleksii <aleksey.shvoruk@gmail.com>
2025-02-16 12:31:48 +0000

    Refactor AuthorizationService into SecurityUtil
    Replaced AuthorizationService with SecurityUtil to simplify and clarify responsibilities. Moved the `getCurrentUser` method and eliminated unused imports and dependencies.

---
+++ src/main/java/dev/oleksii/rotamanagementapp/security/SecurityUtil.java
@@ -0,0 +1,23 @@
+package dev.oleksii.rotamanagementapp.security;
+
+import dev.oleksii.rotamanagementapp.domain.entities.User;
+import dev.oleksii.rotamanagementapp.domain.repos.UserRepository;
+import dev.oleksii.rotamanagementapp.exceptions.UserNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.security.Principal;
+
+@Component
+public class SecurityUtil {
+
+    private final UserRepository userRepository;
+
+    public SecurityUtil(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User getCurrentUser(Principal principal) {
+        return userRepository.findByEmail(principal.getName())
+                .orElseThrow(() -> new UserNotFoundException("User with email: " + principal.getName() + " not found"));
+    }
+}
--- src/main/java/dev/oleksii/rotamanagementapp/services/AuthorizationService.java
+++
@@ -1,9 +0,0 @@
-package dev.oleksii.rotamanagementapp.services;
-
-import dev.oleksii.rotamanagementapp.domain.entities.User;
-
-import java.security.Principal;
-
-public interface AuthorizationService {
-    User getCurrentUser(Principal principal);
-}
