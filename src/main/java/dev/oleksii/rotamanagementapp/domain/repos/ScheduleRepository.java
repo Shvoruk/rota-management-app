6bb6b1cec89450e0f8afc51c13823fef94afb7ae
Oleksii <aleksey.shvoruk@gmail.com>
2025-02-16 15:15:23 +0000

    Remove unused methods and queries from repositories
    Simplified repository interfaces by eliminating unused methods and custom queries.

--- src/main/java/dev/oleksii/rotamanagementapp/domain/repos/ScheduleRepository.java
+++ src/main/java/dev/oleksii/rotamanagementapp/domain/repos/ScheduleRepository.java
@@ -2,8 +2,6 @@ package dev.oleksii.rotamanagementapp.domain.repos;
 
 import dev.oleksii.rotamanagementapp.domain.entities.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,7 +9,5 @@ import java.util.UUID;
 
 @Repository
 public interface ScheduleRepository extends JpaRepository<Schedule, UUID> {
-    @Query("SELECT s.team.id FROM Schedule s WHERE s.id = :scheduleId")
-    UUID findTeamIdByScheduleId(@Param("scheduleId") UUID scheduleId);
     Optional<Schedule> findByTeamId(UUID teamId);
 }
--- src/main/java/dev/oleksii/rotamanagementapp/domain/repos/ShiftRepository.java
+++ src/main/java/dev/oleksii/rotamanagementapp/domain/repos/ShiftRepository.java
@@ -2,14 +2,10 @@ package dev.oleksii.rotamanagementapp.domain.repos;
 
 import dev.oleksii.rotamanagementapp.domain.entities.Shift;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
 public interface ShiftRepository extends JpaRepository<Shift, UUID> {
-    @Query("SELECT s.schedule.team.id FROM Shift s WHERE s.id = :shiftId")
-    UUID findTeamIdByShiftId(@Param("shiftId") UUID shiftId);
 }
