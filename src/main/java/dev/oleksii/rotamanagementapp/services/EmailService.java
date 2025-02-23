684723ba16b6224b8cdc9873fc10e5efed8e87cf
Oleksii <aleksey.shvoruk@gmail.com>
2025-02-23 12:58:17 +0000

    feat: generic sendEmail method is replaced by sendVerificationEmail, which is now accepts template name as param

--- src/main/java/dev/oleksii/rotamanagementapp/services/EmailService.java
+++ src/main/java/dev/oleksii/rotamanagementapp/services/EmailService.java
@@ -1,5 +1,18 @@
 package dev.oleksii.rotamanagementapp.services;
 
+/**
+ * Service interface for sending various types of emails. In this context,
+ * it focuses on sending a verification email, but can be extended as needed.
+ */
 public interface EmailService {
-    void sendEmail(String to, String subject, String body);
+
+    /**
+     * Sends a verification email to a given recipient using a specified template.
+     *
+     * @param to             The email address of the recipient.
+     * @param subject        The subject line of the email.
+     * @param templateName   The name of the Thymeleaf template to render.
+     * @param verificationLink The link to be inserted in the email for verification.
+     */
+    void sendVerificationEmail(String to, String subject, String templateName, String verificationLink);
 }
