89ff64abe670dc2dc6acd9b8a804ef1017e10963
Oleksii <aleksey.shvoruk@gmail.com>
2025-02-23 19:21:39 +0000

    test: finish user repo test

--- src/test/java/dev/oleksii/rotamanagementapp/repos/UserRepositoryTest.java
+++ src/test/java/dev/oleksii/rotamanagementapp/repos/UserRepositoryTest.java
@@ -4,6 +4,7 @@ import dev.oleksii.rotamanagementapp.domain.entities.User;
 import dev.oleksii.rotamanagementapp.domain.entities.VerificationToken;
 import dev.oleksii.rotamanagementapp.domain.enums.Role;
 import dev.oleksii.rotamanagementapp.domain.repos.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -18,11 +19,13 @@ public class UserRepositoryTest {
     @Autowired
     UserRepository userRepository;
 
-    @Test
-    void testFindByVerificationToken() {
+    private User user;
+    private String token;
 
-        String token = UUID.randomUUID().toString();
-        User user = User.builder()
+    @BeforeEach
+    void setUp() {
+        token = UUID.randomUUID().toString();
+        user = User.builder()
                 .fullName("Test")
                 .password("password")
                 .email("test@email.com")
@@ -31,13 +34,31 @@ public class UserRepositoryTest {
                 .verified(false)
                 .verificationToken(new VerificationToken(token, LocalDateTime.now()))
                 .build();
+    }
 
+    @Test
+    void testFindByVerificationToken() {
         userRepository.save(user);
-
         Optional<User> userOptional = userRepository.findByVerificationToken(token);
 
         assert userOptional.isPresent();
         assert userOptional.get().getId().equals(user.getId());
+        assert userOptional.get().getEmail().equals(user.getEmail());
+    }
+
+    @Test
+    void testFindByEmail() {
+        userRepository.save(user);
+        Optional<User> userOptional = userRepository.findByEmail(user.getEmail());
+        assert userOptional.isPresent();
+        assert userOptional.get().getId().equals(user.getId());
         assert userOptional.get().getEmail().equals("test@email.com");
+    }
+
+    @Test
+    void testExistsByEmail() {
+        userRepository.save(user);
+        boolean exists = userRepository.existsByEmail(user.getEmail());
+        assert exists;
     }
 }
